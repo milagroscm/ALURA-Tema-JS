607410cbc6444bd12f19efd67f8e2688c2889135
milagroscm <mcruzmam@unsa.edu.pe>
2022-07-06 21:17:02 -0500

    modificacion de js

---
+++ 1746-JavaScript-parte1-aula0/js/form.js
@@ -0,0 +1,32 @@
+let botonAdicionar = document.querySelector("#adicionar-paciente");
+botonAdicionar.addEventListener("click", function(evento){
+    evento.preventDefault();
+
+    let form = document.querySelector("#form-adicionar");
+    let nombre = form.nombre.value;
+    let peso = form.peso.value;
+    let altura = form.altura.value;
+    let gordura = form.gordura.value;
+    
+    let tabla = document.querySelector("#tabla-pacientes");
+
+    pacienteTr = document.createElement("tr");
+
+    nombreTd = document.createElement("td");
+    pesoTd = document.createElement("td");
+    alturaTd = document.createElement("td");
+    gorduraTd = document.createElement("td");
+    imcTd = document.createElement("td");
+
+    nombreTd.textContent = nombre;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    tabla.appendChild(pacienteTr);
+});
